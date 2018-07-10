92f734161d80a32a4eb95d71a2663202272ae360
Matthew Lee <matt@kynelee.com>
2018-07-10 16:33:02 -0400

    24 explanation

--- ch2/24.scm
+++ ch2/24.scm
@@ -1,6 +1,10 @@
+; cons returns a function takes a function as an arg, and applies the
+; function to the pair x, y 
 (define (cons x y)
   (lambda (m) (m x y)))
 
+; lambda (p q) is defined as the lambda(m) function in cons, and is
+; evaluated with the args x y
 (define (car z)
   (z (lambda (p q) p)))
 
